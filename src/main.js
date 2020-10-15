e61ac4d892f24968a5921fc31cc63b3bec8a49f9
王杰 <wangjie@azsy.cn>
2020-10-15 15:24:32 +0800

    start

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui'
+// import formCreate from '@form-create/element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+// Vue.use(formCreate)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div><form-create /></div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import formCreate from '@form-create/element-ui'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    formCreate: formCreate.$form()
+  },
+  data() {
+    return {
+
+    }
   }
 }
 </script>
